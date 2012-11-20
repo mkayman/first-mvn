3674d7271882e45b22cbfd0066c7db2fccea1c90
meskay <kaymanmesut@gmail.com>
2012-11-20 21:38:49 +0200

    Maven project is created

---
+++ src/main/java/com/mkayman/mvnbook/App.java
@@ -0,0 +1,13 @@
+package com.mkayman.mvnbook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
